a7329652080963d6f801a467bc528b1679c4047a
JPEAL79 <jpleal79@gmail.com>
2024-06-11 22:23:24 -0400

    A conditional was added in the OrderService class to validate the stock of the products

--- orders-service/src/main/java/com/juanpabloleal/orders_service/services/OrderService.java
+++ orders-service/src/main/java/com/juanpabloleal/orders_service/services/OrderService.java
@@ -9,10 +9,9 @@ import com.juanpabloleal.orders_service.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.UUID;
 
-import static org.springframework.http.RequestEntity.post;
+
 
 
 @Service
@@ -22,24 +21,26 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
 
         //Check for inventory
-        this.webClientBuilder.build()
+        BaseResponse result = this.webClientBuilder.build()
                 .post()
                 .uri("http://localhost:8083/api/inventory/in-stock")
                 .bodyValue(orderRequest.getOrderItems())
                 .retrieve()
                 .bodyToMono(BaseResponse.class)
                 .block();
-
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setOrderItems(orderRequest.getOrderItems().stream()
-                .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
-                .toList());
-                this.orderRepository.save(order);
-
+        if (result != null && !result.hasErrors()) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setOrderItems(orderRequest.getOrderItems().stream()
+                    .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
+                    .toList());
+            this.orderRepository.save(order);
+        }else {
+            throw new IllegalArgumentException("Some of the products are not in stock");
+        }
     }
 
     private OrderItems mapOrderItemRequestToOrderItem(OrderItemRequest orderItemRequest, Order order) {
